b6ace38e1f30e7b77775450889441b21ea0da77b
schmoeren <soerenscholz0@gmail.com>
2025-01-25 22:34:54 +0100

    Update FtoC_and_CtoF.cpp

--- FtoC_and_CtoF.cpp
+++ FtoC_and_CtoF.cpp
@@ -7,13 +7,14 @@ int main()
 	cout << "Moechten Sie einen Fahrenheit- oder Celsiuswert umwandeln? 1 fuer Fahrenheit in Celsius und 2 fuer Celsius in Fahrenheit" << endl;
 	cin >> e;
 
-	switch (e)
+	switch (e)			//switch(case) darf nur ein char oder ein int sein
 	{
 	case(1): 
 		cout << "Geben Sie den Fahrenheitwert an, welcher umgewandelt werden soll." << endl;
 		cin >> f;
 		cout << "Der umgewandelte Wert betraegt " << (f - 32) / 1.8 << "C Celsius." << endl;
-		break;
+		break;			//Sobald ein case eintritt wird mit break die weitere abfrage des switches gestoppt. Sobald e=1 ist, wird die abfrage ab 1 gestoppt, 
+					//heisst: case2 und default werden nicht angesprochen = ressourcensparender
 	case(2):
 		cout << "Geben Sie den Celsiuswert an, welcher umgewandelt werden soll." << endl;
 		cin >> c;
